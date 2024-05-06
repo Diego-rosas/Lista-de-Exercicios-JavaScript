85cce6082b82f71969d7deeee22539bd75eca292
Diego Rosas <diegorosas19@gmail.com>
2024-05-06 00:46:09 -0300

    Exercicio 04

--- PASSO 01 - SEQUÊNCIAS BÁSICAS/04/04.js
+++ PASSO 01 - SEQUÊNCIAS BÁSICAS/04/04.js
@@ -3,3 +3,18 @@
 // Autor : Diego Rosas
 // Data : 05/05/2024
 // Seção de Declarações 
+
+const readline = require ('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('Digite um numero: ', (numero1) =>{
+    rl.question('Digite outro numero: ',(numero2) =>{
+        const soma = parseInt(numero1) + parseInt(numero2);
+        console.log('A soma entre', numero1, 'e', numero2,'eh igual a:',soma)
+        rl.close();
+    });
+});
