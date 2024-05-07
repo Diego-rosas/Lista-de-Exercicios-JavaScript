24fdd474230b13450710d4b41741daa173bf46c7
Diego Rosas <diegorosas19@gmail.com>
2024-05-07 16:21:18 +0000

    Exercicio 05

---
+++ PASSO 01 - SEQUÊNCIAS BÁSICAS/05/05.js
@@ -0,0 +1,21 @@
+// Exercicio 05
+// Função : Calcula a media de duas notas e mostra o resultado.
+// Autor :
+// Data : 20/02/2021
+// Seção de Declarações 
+
+const readline = require ('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('Nota 1: ', (nota1) => {
+    rl.question('Nota 2: ', (nota2) => {
+        const soma = parseFloat(nota1) + parseFloat(nota2);
+        const media = soma/2;
+        console.log('A média entre', nota1, 'e', nota2,'eh igual a',media);
+        rl.close();
+    });    
+});
