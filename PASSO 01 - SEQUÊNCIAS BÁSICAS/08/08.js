f141fd9bd78c0aa8bd4344567978f5fa257167a3
Diego Rosas <diegorosas19@gmail.com>
2024-05-10 16:14:52 +0000

    Exercicio 8 - parte 01

---
+++ PASSO 01 - SEQUÊNCIAS BÁSICAS/08/08.js
@@ -0,0 +1,27 @@
+// Exercicio 08
+// Função : Programa que lê um numero que representa uma distância em metros da entrada e mostra os valores relativos em outras medidas.
+// Autor : Diego Rosas
+// Data : 10/05/2024
+// Seção de Declarações 
+
+const readline = require ('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('Digite um numero: ', (distancia) => {
+    const distanciaKm = parseFloat(distancia) / 1000;
+    const distanciaKm = parseFloat(distancia) / 1000;
+    const distanciaKm = parseFloat(distancia) / 1000;
+    const distanciaKm = parseFloat(distancia) / 1000;
+    const distanciaKm = parseFloat(distancia) / 1000;
+    const distanciaKm = parseFloat(distancia) / 1000;
+    const distanciaKm = parseFloat(distancia) / 1000;
+    
+    console.log("O dobro de", numero,"eh", dobro);
+    console.log("A terça parte de", numero,"eh", terca);
+    console.log();
+    rl.close();
+});
