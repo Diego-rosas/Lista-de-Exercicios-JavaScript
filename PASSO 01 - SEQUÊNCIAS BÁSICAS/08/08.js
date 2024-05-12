acc9268b1134da1947f117babbb7082378a98df0
Diego Rosas <diegorosas19@gmail.com>
2024-05-12 22:01:19 +0000

    Exercício  08 - parte 2

--- PASSO 01 - SEQUÊNCIAS BÁSICAS/08/08.js
+++ PASSO 01 - SEQUÊNCIAS BÁSICAS/08/08.js
@@ -11,17 +11,21 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
-rl.question('Digite um numero: ', (distancia) => {
+rl.question("Digite uma distância em metros: ", (distancia) => {
     const distanciaKm = parseFloat(distancia) / 1000;
-    const distanciaKm = parseFloat(distancia) / 1000;
-    const distanciaKm = parseFloat(distancia) / 1000;
-    const distanciaKm = parseFloat(distancia) / 1000;
-    const distanciaKm = parseFloat(distancia) / 1000;
-    const distanciaKm = parseFloat(distancia) / 1000;
-    const distanciaKm = parseFloat(distancia) / 1000;
-    
-    console.log("O dobro de", numero,"eh", dobro);
-    console.log("A terça parte de", numero,"eh", terca);
+    const distanciaHm = parseFloat(distancia) / 100;
+    const distanciaDam = parseFloat(distancia) / 10;
+    const distanciaDm = parseFloat(distancia) * 10;
+    const distanciaCm = parseFloat(distancia) * 100;
+    const distanciaMm = parseFloat(distancia) * 1000;
+
+    console.log("A distância de", distancia,"m corresponde a: ");
     console.log();
+    console.log(distanciaKm,"km");
+    console.log(distanciaHm,"Hm");
+    console.log(distanciaDam,"Dam");
+    console.log(distanciaDm,"Dm");
+    console.log(distanciaCm,"Cm");
+    console.log(distanciaMm,"Mm");
     rl.close();
 });
